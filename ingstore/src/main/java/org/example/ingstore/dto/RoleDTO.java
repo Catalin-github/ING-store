2ca4e92c7d5c4cea1ae679c65078a6c558414fef
Samuel Catalin Moholea <moholea.s00@gmail.com>
2025-07-05 20:58:38 +0300

    feat/Implement Role data transfer object

---
+++ ingstore/src/main/java/org/example/ingstore/dto/RoleDTO.java
@@ -0,0 +1,13 @@
+package org.example.ingstore.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class RoleDTO {
+    private Long id;
+    private String name;
+}
---
+++ ingstore/src/main/java/org/example/ingstore/dto/RoleRequestDTO.java
@@ -0,0 +1,12 @@
+package org.example.ingstore.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class RoleRequestDTO {
+    private String name;
+}
