a1cfed660ff9836b842ed5266b1661d688f52acd
Samuel Catalin Moholea <moholea.s00@gmail.com>
2025-07-05 20:16:19 +0300

    feat/Implement User entity with fields and getters/setters

--- ingstore/src/main/java/org/example/ingstore/entity/User.java
+++ ingstore/src/main/java/org/example/ingstore/entity/User.java
@@ -2,12 +2,20 @@ package org.example.ingstore.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 
 import java.io.Serial;
 import java.io.Serializable;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(onlyExplicitlyIncluded = true)
 public class User implements Serializable {
 
     @Serial
@@ -15,10 +23,14 @@ public class User implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    @ToString.Include
     private Long id;
 
     @NotBlank
     @Column(nullable = false, unique = true)
+    @EqualsAndHashCode.Include
+    @ToString.Include
     private String username;
 
     @NotBlank
@@ -27,67 +39,6 @@ public class User implements Serializable {
 
     @NotBlank
     @Column(nullable = false)
+    @ToString.Exclude
     private String password;
-
-//    @ManyToMany(fetch = FetchType.EAGER)
-//    @JoinTable(
-//            name = "user_roles",
-//            joinColumns = @JoinColumn(name = "user_id"),
-//            inverseJoinColumns = @JoinColumn(name = "role_id")
-//    )
-//    private Set<Role> roles;
-
-    // Default constructor
-    public User() {
-    }
-
-    // Parameterized constructor
-    public User(String username, String password
-//                Set<Role> roles
-    ) {
-        this.username = username;
-        this.password = password;
-//        this.roles = roles;
-    }
-
-    // Getters and setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-//    public Set<Role> getRoles() {
-//        return roles;
-//    }
-
-//    public void setRoles(Set<Role> roles) {
-//        this.roles = roles;
-//    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
